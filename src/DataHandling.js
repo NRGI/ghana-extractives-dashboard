62ac75c39d5f9c799f5bd87e452732945334f132
yannaungoak <yannaungoak@gmail.com>
2018-09-08 13:09:44 +0630

    reshaped data obatined from sources and change to sheetsy instead of tabletop

--- src/DataHandling.js
+++ src/DataHandling.js
@@ -1,44 +1,11 @@
 import * as d3 from 'd3';
 import _ from 'lodash';
-import Tabletop from 'tabletop';
-
-// const cleanIncomes = (d) => ({
-//   countyName: d['Name'],
-//   USstate: d['State'],
-//   medianIncome: Number(d['Median Household Income']),
-//   lowerBound: Number(d['90% CI Lower Bound']),
-//   upperBound: Number(d['90% CI Upper Bound'])
-// });
-
-// const dateParse = d3.timeParse("%m/%d/%Y");
-
-// const cleanSalary = (d) => {
-//   if (!d['base salary'] || Number(d['base salary']) > 300000) {
-//     return null;
-//   }
-
-//   return {employer: d.employer,
-//           submit_date: dateParse(d['submit date']),
-//           start_date: dateParse(d['start date']),
-//           case_status: d['case status'],
-//           job_title: d['job title'],
-//           clean_job_title: d['job title'],
-//           base_salary: Number(d['base salary']),
-//           city: d['city'],
-//           USstate: d['state'],
-//           county: d['county'],
-//           countyID: d['countyID']
-//   };
-// }
-
-// const cleanUSStateName = (d) => ({
-//   code: d.code,
-//   id: Number(d.id),
-//   name: d.name
-// });
+// import Tabletop from 'tabletop';
+import sheetsy from 'sheetsy';
 
 export const loadAllData = () => {
 
+  const { urlToKey, getWorkbook, getSheet } = sheetsy;
 
   const files = [
     "https://www.resourcedata.org/dataset/e4f9dc35-7b68-4b41-90ee-3e2411e2fd8f/resource/9ddd590c-7d62-42b4-8744-868c481d58ae/download/company-payments-ghana.csv", 
@@ -54,42 +21,95 @@ export const loadAllData = () => {
     promises.push(d3.csv(url))
   });
   
+  /* Get commodities data from Google sheet using sheetsy
+  */
   promises.push(
-    Tabletop.init( { key: publicSpreadsheetUrl,
-      callback: (data) => promises.push(data),
-      simpleSheet: true 
-    })
+    getWorkbook(urlToKey(publicSpreadsheetUrl))
+    .then((workBook) => getSheet(
+        urlToKey(publicSpreadsheetUrl),
+        workBook.sheets[0].id
+      )
+    )
   )
 
+  const reshapeCompanyPayments = (data) => {
+    const reshapedData = data.map(d => {
+      console.log(handleGHSConversion(+d.currency_rate,+d.year), +d.currency_rate, +d.year);
+      return {
+        company_name: d.company_name,
+        currency_rate: handleGHSConversion(+d.currency_rate,+d.year),
+        value_reported: handleGHSConversion(+d.value_reported,+d.year),
+        value_reported_as_USD: +d.value_reported_as_USD,
+        year: +d.year,
+        name_of_revenue_stream: d.name_of_revenue_stream,
+        gfs_code: d.gfs_code,
+        gfs_description: d.gfs_description,
+        org_id: getOrgId(d.reporting_url),
+      } 
+    });
+    return reshapedData;
+  }
+
+  const reshapeGovtAgencies = (data) => {
+    const reshapedData = data.map(d => {
+      return {
+        government_agency_name: d.government_agency_name,
+        currency_rate: handleGHSConversion(+d.currency_rate,+d.year),
+        value_reported: handleGHSConversion(+d.value_reported,+d.year),
+        value_reported_as_USD: +d.value_reported_as_USD,
+        year: +d.year,
+        name_of_revenue_stream: d.name_of_revenue_stream,
+        gfs_code: d.gfs_code,
+        gfs_description: d.gfs_description,
+        org_id: getOrgId(d.reporting_url),
+      } 
+    });
+    return reshapedData;
+  }
+
+  const reshapeCommodities = (data) => {
+    let reshapedData = [];
+    
+    data.rows.forEach(d => {
+      reshapedData = reshapedData.concat( 
+        d.year.split(',').map(year => {
+          return {
+            year: +year,
+            commodity: d.commodity,
+            original_name: d.originalname,
+            updated_name: d.updatedname
+          } 
+        })
+      )
+    })
+    return reshapedData;
+  }
+
+  // const snakeCaseConverter = d => d.replace(' ','_').toLowerCase();
+
+  /** This converts second generation Ghanian  
+   *  cedis to third gen Ghanian cedis. In 2007
+   *  Ghana revalued its currency so that 
+   *  10,000 GHS = 1 GHS.
+   *  First EITI report that used third gen GHS
+   *  was 2005 report, published in 2008
+   *  https://eiti.org/document/2005-ghana-eiti-report 
+  */
+  const handleGHSConversion = (ghs,year) => year <= 2004 ? ghs/10000 : ghs;
+
+  /** Gets organisation ID from the last bit of 
+   *  the EITI API access URL.
+  */
+  const getOrgId = url => url.split("/").slice(-1)[0];
   
   return Promise.all(promises).then(function(values) {
     const result = {};  
-    result.companyPayments = values[0];
-    result.govtAgencies = values[1];
-    result.commoditiesTabletopObj = values[2];
+    result.companyPayments = reshapeCompanyPayments(values[0]);
+    result.govtAgencies = reshapeGovtAgencies(values[1]);
+    result.commodities = reshapeCommodities(values[2]);
     console.log(result);
+
     return result;
   });
-  
-  // return data;
-  
-  // d3.queue()
-  //   .defer(d3.csv, '')
-  //   .defer(d3.csv, '')
-  //   .await((error, companyPayments, govtAgencies) => {
-  
-  //     console.log(companyPayments);
-  //     console.log(govtAgencies);
 
-  // // callback({
-  // //     usTopoJson: us,
-  // //     countyNames: countyNames,
-  // //     medianIncomes: medianIncomesMap,
-  // //     medianIncomesByCounty: _.groupBy(medianIncomes, 'countyName'),
-  // //     medianIncomesByUSState: _.groupBy(medianIncomes, 'USstate'),
-  // //     techSalaries: techSalaries,
-  // //     USstateNames: USstateNames
-  // //   });
-
-  //   });
 };
