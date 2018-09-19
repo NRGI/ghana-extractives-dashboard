601f8c26eec53523ee60007f442631b96f5371f3
yannaungoak <yannaungoak@gmail.com>
2018-09-19 10:47:05 +0800

    stacked bar chart commit

---
+++ src/components/StackedBarChart/StackedBarChart.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import StackedBarChart from './StackedBarChart'
+
+storiesOf('StackedBarChart', module).add('Example 1', () =>
+  <StackedBarChart />
+)
